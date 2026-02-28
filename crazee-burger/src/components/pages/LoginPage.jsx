edc25290e94b715b0f3df8ca5c7cf5e16391e260
ZekriAyoub <ayoub.zekri.3@icloud.com>
2026-02-28 17:09:07 +0100

    F01 : Login > add title and form

--- crazee-burger/src/components/pages/LoginPage.jsx
+++ crazee-burger/src/components/pages/LoginPage.jsx
@@ -1,6 +1,38 @@
+import { useState } from "react";
 
 export default function LoginPage() {
-  return (
-    <div>LoginPage</div>
-  )
+  // state
+  const [inputValue, setInputValue] = useState("")
+  
+  //comportements
+  const handleChange = (event) => {
+    setInputValue(event.target.value)
+
+  }
+
+  const handleSubmit = (event) => { 
+    event.preventDefault()
+    setInputValue(inputValue)
+    alert(`Bonjour ${inputValue} !`)
+    setInputValue("")
+   }
+
+
+  //render
+  return(
+    <div>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          type="text" 
+          placeholder="Entrez votre prénom..." 
+          required 
+          value={inputValue} 
+          onChange={handleChange}/>
+        <button>Accédez à votre espace</button>
+      </form>
+    </div>
+    )
 }
