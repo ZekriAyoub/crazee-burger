1efafd7bffcd5857c6d60fc783c483a4d7d6ba02
ZekriAyoub <ayoub.zekri.3@icloud.com>
2026-02-28 17:20:04 +0100

    F01 : refacto login

---
+++ crazee-burger/src/components/pages/login/LoginForm.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+export default function LoginForm() {
+ // state
+  const [inputValue, setInputValue] = useState("")
+  
+  //comportements
+  const handleChange = (event) => {
+    setInputValue(event.target.value)
+
+  }
+
+  const handleSubmit = (event) => { 
+    event.preventDefault()
+    setInputValue(inputValue)
+    alert(`Bonjour ${inputValue} !`)
+    setInputValue("")
+   }
+
+  return (
+    <form action="submit" onSubmit={handleSubmit}>
+    <h1>Bienvenue chez nous</h1>
+    <br />
+    <h2>Connectez-vous</h2>
+      <input
+        type="text" 
+        placeholder="Entrez votre prénom..." 
+        required 
+        value={inputValue} 
+        onChange={handleChange}/>
+      <button>Accédez à votre espace</button>
+    </form>
+    
+  )
+}
